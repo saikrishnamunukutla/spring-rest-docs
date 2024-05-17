35e2a3d9953b5769a3c723a4973a57228e0fa0b0
Sai Krishna Munukutla <saikrishnamunukutla@sais-mbp.kpn>
2024-05-17 14:42:10 +0200

    initial commit with docs

--- src/test/java/com/sk/springrestdocs/docs/CustomerIntegrationTests.java
+++ src/test/java/com/sk/springrestdocs/docs/CustomerIntegrationTests.java
@@ -14,11 +14,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.payload.FieldDescriptor;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -49,14 +48,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class CustomerIntegrationTests {
 
     @Container
+    @ServiceConnection
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0").withExposedPorts(27017);
 
-    @DynamicPropertySource
-    static void mongoDbProperties(DynamicPropertyRegistry registry) {
-
-        mongoDBContainer.start();
-        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-    }
 
 
     @Autowired
